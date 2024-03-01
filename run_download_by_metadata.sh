8915adc9b6610c206d7bc8420cd2aa24d34c1634
alefiury <alefiury14@gmail.com>
2024-03-01 14:43:53 -0300

    Bash file bug fix

--- run_download_by_metadata.sh
+++ run_download_by_metadata.sh
@@ -1,7 +1,8 @@
-root_path="audioset_unbalanced/"
-n_jobs=1
-download_type="unbalanced_train" # (unbalanced_train, balanced_train, eval)
+root_path="audioset_unbalanced/" # output path
+n_jobs=12 # num threads
+metadata_path=""
+
 format="wav"
 quality=0
 
-python3 download.py --root-path=$root_path --n-jobs=$n_jobs --download-type=$download_type --format=$format --quality=$quality
+python3 download.py --root-path=$root_path --n-jobs=$n_jobs --metadata-path=$metadata_path --format=$format --quality=$quality
--- run_download_by_type.sh
+++ run_download_by_type.sh
@@ -1,7 +1,7 @@
 root_path="audioset_unbalanced/"
-n_jobs=12
-metadata_path=""
+n_jobs=1
+download_type="unbalanced_train" # (unbalanced_train, balanced_train, eval)
 format="wav"
 quality=0
 
-python3 download.py --root-path=$root_path --n-jobs=$n_jobs --metadata-path=$metadata_path --format=$format --quality=$quality
+python3 download.py --root-path=$root_path --n-jobs=$n_jobs --download-type=$download_type --format=$format --quality=$quality
